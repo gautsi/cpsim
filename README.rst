f0d1231fca1115791276b3042bd1f8c67c7507f6
Gautam <gautam.sisodia@gmail.com>
2015-10-29 21:58:53 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,12 @@
+===============================
+cp simulations
+===============================
+
+.. image:: https://img.shields.io/travis/gautsi/cpsim.svg
+        :target: https://travis-ci.org/gautsi/cpsim
+
+cp simulations
+
+* Free software: BSD license
+* Documentation: https://cpsim.readthedocs.org.
+
---
+++ tests/test_cpsim.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_cpsim
+----------------------------------
+
+Tests for `cpsim` module.
+"""
+
+import unittest
+
+from cpsim import cpsim
+
+
+class TestCpsim(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
